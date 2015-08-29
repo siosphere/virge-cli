b099dc298de414f842b8d06e2efd2149b19955cd
Kramer <kramer@siosphere.com>
2015-08-28 19:49:58 -0700

    Setup terminate and sleep commands to help with unit testing of commands

--- Virge/Cli/Component/Command.php
+++ Virge/Cli/Component/Command.php
@@ -8,4 +8,21 @@ use Virge\Core\Model;
  * @author Michael Kramer
  */
 class Command extends Model{
+    
+    /**
+     * Terminate the command
+     * @param int $exitCode
+     */
+    protected function terminate($exitCode = 0){
+        exit($exitCode);
+    }
+    
+    /**
+     * Have the command sleep by seconds
+     * @param int $seconds
+     */
+    protected function sleep($seconds = 0){
+        sleep($seconds);
+    }
+    
 }
