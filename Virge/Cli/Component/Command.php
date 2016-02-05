6a865a4db33254d49b1031ab8f4ac537ae41cfe3
Kramer <kramer@siosphere.com>
2016-02-05 09:37:00 -0800

    Added support for command lock files Updated composer.json to not have vcs directly

--- Virge/Cli/Component/Command.php
+++ Virge/Cli/Component/Command.php
@@ -2,12 +2,17 @@
 namespace Virge\Cli\Component;
 
 use Virge\Core\Model;
+use Virge\Enigma;
 
 /**
  * 
  * @author Michael Kramer
  */
 class Command extends Model{
+    
+    const COMMAND = ''; //should be overwritten by each command
+    
+    protected $lockValue = null;
     
     /**
      * Terminate the command
@@ -23,6 +28,40 @@ class Command extends Model{
      */
     protected function sleep($seconds = 0){
         sleep($seconds);
+    }
+    
+    /**
+     * Is an instance of this command already running?
+     * @return type
+     */
+    protected function instanceAlreadyRunning() {
+        
+        $running_processes = array();
+        exec("ps aux | grep '".self::COMMAND."' | grep -v grep", $running_processes);
+        return count($running_processes) > 1;
+    }
+    
+    /**
+     * Returns true if the lock file matches, false otherwise
+     * 
+     * @param string $file - Absolute path to lock file
+     * @return boolean
+     */
+    protected function getCommandLock($file)
+    {
+        if(!is_file($file)) {
+            $lockValue = Enigma::hash(microtime());
+            file_put_contents($file, $lockValue);
+        } else {
+            $lockValue = file_get_contents($file);
+        }
+        
+        if($this->lockValue === null) {
+            $this->lockValue = $lockValue;
+            return true;
+        }
+        
+        return $this->lockValue === $lockValue;
     }
     
 }
